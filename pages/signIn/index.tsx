430af3b28ab3e925d916620b6e07212caa22cda5
iplatos <platonoff9168@gmail.com>
2023-10-27 11:46:57 +0300

    test2

--- pages/signIn/index.tsx
+++ pages/signIn/index.tsx
@@ -4,10 +4,15 @@ import {HeadMeta} from "@/components/HeadMeta/HeadMeta";
 
 function LogIn () {
     return (
-        <>
+      /*  <>
             <HeadMeta title={"Sign in"}/>
            Sign In page!!!!!!
+        </>*/
+        <>
+        <input style={{border:"30px red solid"}} />
         </>
+
+
     );
 };
 
