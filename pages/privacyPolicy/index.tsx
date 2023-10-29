c3dc6105823076e108328f258db1361c0f84a5b8
iplatos <platonoff9168@gmail.com>
2023-10-29 23:50:38 +0300

    feat: refactor TextField

--- pages/privacyPolicy/index.tsx
+++ pages/privacyPolicy/index.tsx
@@ -1,23 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { HeadMeta } from '@/components/HeadMeta/HeadMeta';
 import { getLayout } from '@/components/Layout/Layout';
-import { TextField } from '@/components/textField/textField';
 
 function PrivacyPolicy() {
-  const [value, setValue] = useState('as');
-
   return (
     <>
       <HeadMeta title={' Terms of Service'} />
       Privacy Policy
-      <TextField
-        as={'textarea'}
-        inputType={'password'}
-        label={'label'}
-        onChangeValue={setValue}
-        value={value}
-      />
     </>
   );
 }
