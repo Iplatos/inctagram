e3cab525c995e948a9e59b06737c0b1b90e9f82d
iplatos <platonoff9168@gmail.com>
2023-10-31 10:11:22 +0300

    fix:  TextField button

--- pages/privacyPolicy/index.tsx
+++ pages/privacyPolicy/index.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { HeadMeta } from '@/components/HeadMeta/HeadMeta';
 import { getLayout } from '@/components/Layout/Layout';
+import { TextField } from '@/components/textField/textField';
 
 function PrivacyPolicy() {
+  const [a, setA] = useState('');
+
   return (
     <>
       <HeadMeta title={' Terms of Service'} />
       Privacy Policy
+      <TextField isSearchInput onChangeValue={setA} value={a} />
     </>
   );
 }
