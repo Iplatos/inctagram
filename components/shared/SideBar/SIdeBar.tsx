555901226a255dace674b4aa929ea23ffe4794ae
iplatos <platonoff9168@gmail.com>
2023-11-01 14:58:07 +0300

    feat: add button for sideBar

--- components/shared/SideBar/SIdeBar.tsx
+++ components/shared/SideBar/SIdeBar.tsx
@@ -1,14 +1,16 @@
 import s from './SideBar.module.scss';
 
-/*import homeOutline from "../../../assets/icons/home-outline.svg"*/
 import home from '../../../assets/icons/home.svg';
+import homeOutline from '../../../assets/icons/home-outline.svg';
 
+console.log(homeOutline);
+console.log(home);
 export const SideBar = () => {
   return (
     <div className={s.sidebarContainer}>
       <div className={s.buttonContainer}>
         <button>
-          <img src={home.src} />
+          <img alt={'home logo'} className={s.image} src={homeOutline.src} />
           Home
         </button>
         <button>Create</button>
