2ce6639d4baca2b43169dcc4d0d04c92502c3733
iplatos <platonoff9168@gmail.com>
2023-11-01 14:19:35 +0300

    feat: add style for sideBar

--- components/shared/SideBar/SIdeBar.tsx
+++ components/shared/SideBar/SIdeBar.tsx
@@ -1,13 +1,28 @@
 import s from './SideBar.module.scss';
 
+/*import homeOutline from "../../../assets/icons/home-outline.svg"*/
+import home from '../../../assets/icons/home.svg';
+
 export const SideBar = () => {
   return (
     <div className={s.sidebarContainer}>
-      <button>asd</button>
-      <button>asd</button>
-      <button>asd</button>
-      <button>asd</button>
-      <button>asd</button>
+      <div className={s.buttonContainer}>
+        <button>
+          <img src={home.src} />
+          Home
+        </button>
+        <button>Create</button>
+        <button>My Profile</button>
+        <button>Messenger</button>
+        <button>Search</button>
+      </div>
+      <div className={s.buttonContainer}>
+        <button>Statistics</button>
+        <button>Favorites</button>
+      </div>
+      <div className={s.logOutButtonContainer}>
+        <button>Log Out</button>
+      </div>
     </div>
   );
 };
