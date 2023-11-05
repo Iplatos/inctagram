630effe6d73f1c5a0d444a91d60fc4679bacb2ab
Marina <marina1497@list.ru>
2023-11-05 18:31:49 +0300

    init: start forgot password task

--- pages/forgotPassword/index.tsx
+++ pages/forgotPassword/index.tsx
@@ -2,12 +2,31 @@ import React from 'react';
 
 import { HeadMeta } from '@/components/HeadMeta/HeadMeta';
 import { getLayout } from '@/components/Layout/Layout';
+import { Card } from '@/shared/ui/Card/Card';
+import { Typography } from '@/shared/ui';
 
 function ForgotPassword() {
   return (
     <>
       <HeadMeta title={'Forgot Password?'} />
-      Forgot Password 1?
+
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          backgroundColor: '#333333',
+          padding: '24px',
+        }}
+      >
+        <Typography.H1>Forgot Password</Typography.H1>
+        <form>
+          <input type="password" />
+          <Typography.Regular14>
+            Enter your email address and we will send you further instructions
+          </Typography.Regular14>
+        </form>
+      </div>
     </>
   );
 }
