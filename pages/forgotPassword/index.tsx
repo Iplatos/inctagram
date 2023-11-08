475a137eb89d9a2bb9f8c9929d19fc95ff8107d3
iplatos <platonoff9168@gmail.com>
2023-11-08 17:20:48 +0300

    feat:add radix-ui/react-dropdown-menu

--- pages/forgotPassword/index.tsx
+++ pages/forgotPassword/index.tsx
@@ -3,14 +3,12 @@ import React from 'react';
 import { HeadMeta } from '@/components/HeadMeta/HeadMeta';
 import { getLayout } from '@/components/Layout/Layout';
 import { ForgotPasswordForm } from '@/components/auth/forgot-password-form';
-import { TextField } from '@/shared/ui/textField/TextField';
 
 function ForgotPassword() {
   return (
     <>
       <HeadMeta title={'Forgot Password?'} />
       <ForgotPasswordForm />
-      <TextField isSearchInput value={'as'} />
     </>
     // <div style={{ display: 'flex', justifyContent: 'center', paddingTop: '72px' }}>
 
