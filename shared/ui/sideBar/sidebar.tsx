ef453385b8dc8adb36af777feec8800f3cdc7986
iplatos <platonoff9168@gmail.com>
2023-11-08 18:25:52 +0300

    feat: add svg

--- shared/ui/sideBar/sidebar.tsx
+++ shared/ui/sideBar/sidebar.tsx
@@ -1,5 +1,11 @@
+import BookmarkOutline from 'assets/icons/bookmark-outline.svg';
 import HomeOutline from 'assets/icons/home-outline.svg';
+import LogOutOutline from 'assets/icons/log-out-outline.svg';
+import MessageCircleOutline from 'assets/icons/message-circle-outline.svg';
+import PersonOutline from 'assets/icons/person-outline.svg';
+import PlusSquareOutline from 'assets/icons/plus-square-outline.svg';
 import SearchOutline from 'assets/icons/searchOutline.svg';
+import TrendingUpOutline from 'assets/icons/trending-up-outline.svg';
 
 import s from './sidebar.module.scss';
 
@@ -11,20 +17,38 @@ export const SideBar = () => {
           <HomeOutline className={s.svgAsComponent} />
           Home
         </button>
-        <button>Create</button>
-        <button>My Profile</button>
-        <button>Messenger</button>
+        <button>
+          <PlusSquareOutline className={s.svgAsComponent} />
+          Create
+        </button>
+        <button>
+          <PersonOutline className={s.svgAsComponent} />
+          My Profile
+        </button>
+        <button>
+          <MessageCircleOutline className={s.svgAsComponent} />
+          Messenger
+        </button>
         <button>
           <SearchOutline className={s.svgAsComponent} />
           Search
         </button>
       </div>
       <div className={s.buttonContainer}>
-        <button>Statistics</button>
-        <button>Favorites</button>
+        <button>
+          <TrendingUpOutline className={s.svgAsComponent} />
+          Statistics
+        </button>
+        <button>
+          <BookmarkOutline className={s.svgAsComponent} />
+          Favorites
+        </button>
       </div>
       <div className={s.logOutButtonContainer}>
-        <button>Log Out</button>
+        <button>
+          <LogOutOutline className={s.svgAsComponent} />
+          Log Out
+        </button>
       </div>
     </div>
   );
