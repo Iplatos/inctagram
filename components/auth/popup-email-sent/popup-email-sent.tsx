9b9b98a0c07aa3bf754664fb8073367a07c503df
Marina <marina1497@list.ru>
2023-11-08 20:39:49 +0300

    feat: add popup sent email

--- components/auth/popup-email-sent/popup-email-sent.tsx
+++ components/auth/popup-email-sent/popup-email-sent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Typography } from '@/shared/ui';
 import { Button } from '@/shared/ui/Button/button';
 import * as Dialog from '@radix-ui/react-dialog';
 import Image from 'next/image';
@@ -7,46 +8,46 @@ import Image from 'next/image';
 import style from './popup-email-sent.module.scss';
 
 import close from '../../../assets/icons/close.svg';
-import { Card } from '@/shared/ui/Card/Card';
-import { Typography } from '@/shared/ui';
+
+// import { Card } from '@/shared/ui/Card/Card';
 
 export const PopupEmailSent = () => {
   return (
-    <div>
+    <>
       <Dialog.Root>
         <Dialog.Trigger asChild>
-          <button className={style.button}>Edit profile</button>
+          <Button>Edit profile</Button>
         </Dialog.Trigger>
 
         <Dialog.Portal>
           <Dialog.Overlay className={style.dialogOverlay} />
 
           <Dialog.Content className={style.dialogContent}>
-            <div>
+            <div className={style.titleBlock}>
               <Typography.H1 className={style.dialogTitle}>Email sent</Typography.H1>
-            </div>
-            <div></div>
-
-            <Typography.Regular16 className={style.dialogDescription}>
-              We have sent a link to confirm your email to epam com
-            </Typography.Regular16>
-
-            <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: 25 }}>
               <Dialog.Close asChild>
-                <Button className={style.button} variant={'primary'}>
-                  OK
-                </Button>
+                <button aria-label={'Close'} className={style.closeButton}>
+                  <Image alt={'closeModal'} src={close} />
+                </button>
               </Dialog.Close>
             </div>
 
-            <Dialog.Close asChild>
-              <button aria-label={'Close'} className={style.closeButton}>
-                <Image alt={'closeModal'} src={close} />
-              </button>
-            </Dialog.Close>
+            <div className={style.contentBlock}>
+              <Typography.Regular16 className={style.dialogDescription}>
+                We have sent a link to confirm your email to epam com
+              </Typography.Regular16>
+
+              <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: 25 }}>
+                <Dialog.Close asChild>
+                  <Button className={style.button} variant={'primary'}>
+                    OK
+                  </Button>
+                </Dialog.Close>
+              </div>
+            </div>
           </Dialog.Content>
         </Dialog.Portal>
       </Dialog.Root>
-    </div>
+    </>
   );
 };
