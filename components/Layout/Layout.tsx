7b8c7aa86cb5e21d2b0d4127e0ddf93e793de64b
Ekaterina <ushakova.esu@gmail.com>
2023-11-14 23:00:40 +0600

    refactor: change directory for sign up

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,8 +1,8 @@
 import { PropsWithChildren, ReactElement } from 'react';
 import { Provider } from 'react-redux';
 
+import { Header } from '@/components/Header';
 import { NavBar } from '@/components/NavBar/NavBar';
-import { Header } from '@/components/header';
 import { store } from '@/pages/api/store';
 import { NextPage } from 'next';
 
