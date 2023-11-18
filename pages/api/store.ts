96d0c6a91e47c76936cd66426345647c596192f1
User0k <glucus2@yandex.ru>
2023-11-18 22:43:34 +0300

    feat: add useAppSelector, aut reducer

--- pages/api/store.ts
+++ pages/api/store.ts
@@ -1,12 +1,19 @@
+import { TypedUseSelectorHook, useSelector } from 'react-redux';
+
+import authReducer from '@/components/auth/slices/auth';
 import { baseApi } from '@/pages/api/base-api';
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+const rootReducer = combineReducers({
+  authReducer,
+  [baseApi.reducerPath]: baseApi.reducer,
+});
 
 export const store = configureStore({
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
-  reducer: {
-    [baseApi.reducerPath]: baseApi.reducer,
-  },
+  reducer: rootReducer,
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
