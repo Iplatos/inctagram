7123d551629333e7755847c68079f810c7d52318
iplatos <platonoff9168@gmail.com>
2023-11-29 15:13:57 +0300

    feat: add dataPicker

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
+import { useState } from 'react';
+import DatePicker from 'react-multi-date-picker';
+
 import { HeadMeta } from '@/components/HeadMeta/HeadMeta';
 import { getLayout } from '@/components/Layout/Layout';
 import { useGetFeedQuery } from '@/pages/api/base-api';
 import { useTranslation } from '@/shared/hooks/useTranslation';
 import { useRouter } from 'next/navigation';
+import InputIcon from 'react-multi-date-picker/components/input_icon';
+
+import 'react-multi-date-picker/styles/backgrounds/bg-dark.css';
+
+import s from './../components/datePicker/datePicker.module.css';
 
 function Home() {
   const { t } = useTranslation();
@@ -10,7 +18,7 @@ function Home() {
   /*const { data, error } = useFilteredPostsQuery();*/
   const router = useRouter();
   /*const { getMe } = useGetMeQuery();*/
-
+  const [value, setValue] = useState(new Date());
   //   console.log('router.locales: ', router.locales);
   //   console.log('router.locale: ', router.locale);
   //   console.log('router.defaultLocale: ', router.defaultLocale);
@@ -25,6 +33,16 @@ function Home() {
     <>
       <HeadMeta title={'main'} />
       <div style={{ visibility: 'hidden' }}>adf</div>
+
+      <DatePicker
+        className={'bg-dark'}
+        containerClassName={s.cont}
+        headerOrder={['MONTH_YEAR', 'LEFT_BUTTON', 'RIGHT_BUTTON']}
+        inputClass={s.customInput}
+        onChange={setValue}
+        render={<InputIcon />}
+        value={value}
+      />
       {/*  <div>
         {error?.status} {JSON.stringify(error)}
       </div>*/}
