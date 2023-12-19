bf77b28f3c14b9b848c6d49f1c3cd8ba1138829b
Marina <marina1497@list.ru>
2023-12-19 17:05:59 +0300

    init: start IN-113

--- pages/link-expired/index.tsx
+++ pages/link-expired/index.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { HeadMeta } from '@/widgets/HeadMeta/HeadMeta';
 import { getLayout } from '@/widgets/Layout/Layout';
 import { RedirectContent } from '@/widgets/auth';
+import { PaginationItems } from '@/entities/pagination-items/pagination-items';
 
 function LinkExpiredPage() {
   return (
     <>
       <HeadMeta title={'Link Expired'} />
-      <RedirectContent linkExpired />
+      {/* <RedirectContent linkExpired /> */}
+      <PaginationItems />
     </>
   );
 }
--- widgets/header/LangSwitcher/langSwitcher.tsx
+++ widgets/header/LangSwitcher/langSwitcher.tsx
@@ -14,7 +14,8 @@ export const LangSwitcher = () => {
   const defaultIdx = selectOptions.findIndex(option => option.value === locale);
 
   const changeLangHandler = (value: string) => {
-    push({ pathname, query }, asPath, { locale: value });
+    // push({ pathname, query }, asPath, { locale: value });
+    push(value);
   };
 
   return (
