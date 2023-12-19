04b61cc05fdeeff6febe5e24f3d475bb299381b6
Marina <marina1497@list.ru>
2023-12-19 17:06:50 +0300

    feat: change pagination

--- pages/link-expired/index.tsx
+++ pages/link-expired/index.tsx
@@ -3,14 +3,12 @@ import React from 'react';
 import { HeadMeta } from '@/widgets/HeadMeta/HeadMeta';
 import { getLayout } from '@/widgets/Layout/Layout';
 import { RedirectContent } from '@/widgets/auth';
-import { PaginationItems } from '@/entities/pagination-items/pagination-items';
 
 function LinkExpiredPage() {
   return (
     <>
       <HeadMeta title={'Link Expired'} />
-      {/* <RedirectContent linkExpired /> */}
-      <PaginationItems />
+      <RedirectContent linkExpired />
     </>
   );
 }
