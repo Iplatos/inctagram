484abfa8357926200dbecf33b842bb9ca437bd65
ArtyomSamsonchik <colpachek@gmail.com>
2023-12-24 17:33:34 +0300

    feat: add slot-based styling to the Alert common component
    Alert can now retrieve a map object of classes with keys corresponding to component slots.
    Refactor root-slot classname for better readability.

--- shared/ui/alert/alert.tsx
+++ shared/ui/alert/alert.tsx
@@ -1,27 +1,28 @@
-import React, {
-  ElementRef,
-  PropsWithChildren,
-  ReactElement,
-  SyntheticEvent,
-  forwardRef,
-} from 'react';
+import React, { ElementRef, PropsWithChildren, ReactElement, forwardRef } from 'react';
 
 import { CloseIcon } from '@/assets/icons/close';
 import { capitalise } from '@/shared/helpers/capitalise';
+import { clsx } from 'clsx';
 
 import s from './alert.module.scss';
 
-// TODO: add classes feature
-type AlertProps = PropsWithChildren<{
+type AlertSeverity = 'error' | 'success';
+export type AlertSlot = 'action' | 'close' | 'message' | 'root';
+export type AlertClasses = { [P in AlertSlot]?: string };
+
+export type AlertProps = PropsWithChildren<{
   action?: ReactElement;
-  onClose?: (event: SyntheticEvent) => void;
-  severity: 'error' | 'success';
+  classes?: AlertClasses;
+  onClose?: () => void;
+  severity?: AlertSeverity;
 }>;
 
 export const Alert = forwardRef<ElementRef<'div'>, AlertProps>(
-  ({ action, children, onClose, severity }, ref) => {
+  ({ action, children, classes = {}, onClose, severity = 'success' }, ref) => {
+    const cls = getClassNames(classes, severity);
+
     const resolvedAction = action ?? (
-      <button className={s[`close${capitalise(severity)}`]} onClick={e => onClose?.(e)}>
+      <button className={cls.close} onClick={() => onClose?.()}>
         <CloseIcon />
       </button>
     );
@@ -29,10 +30,17 @@ export const Alert = forwardRef<ElementRef<'div'>, AlertProps>(
     const showAction = onClose ?? action;
 
     return (
-      <div className={s[severity]} ref={ref}>
-        <div className={s.message}>{children}</div>
-        <div className={s.action}>{showAction && resolvedAction}</div>
+      <div className={cls.root} ref={ref}>
+        <div className={cls.message}>{children}</div>
+        <div className={cls.action}>{showAction && resolvedAction}</div>
       </div>
     );
   }
 );
+
+const getClassNames = (classes: AlertClasses, severity: AlertSeverity) => ({
+  action: clsx(s.action, classes.action),
+  close: clsx(s[`close${capitalise(severity)}`], classes.close),
+  message: clsx(s.message, classes.message),
+  root: clsx(s[`root${capitalise(severity)}`], classes.root),
+});
