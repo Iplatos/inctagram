1c8f767ad7de7d3b8b58ea9de169015fd14086b0
ArtyomSamsonchik <colpachek@gmail.com>
2024-01-31 00:50:16 +0300

    feat: add error message display in DatePickerContainer calendar
    Formatted and fixed linter errors in the datePicker.module.scss file.

--- components/datePicker/datePickerContainer.tsx
+++ components/datePicker/datePickerContainer.tsx
@@ -1,30 +1,54 @@
-import { ComponentPropsWithoutRef, FC } from 'react';
-import DatePicker from 'react-multi-date-picker';
+import { ComponentPropsWithoutRef, FC, useState } from 'react';
+import DatePicker, { DateObject } from 'react-multi-date-picker';
 
 import { useDatePickerFormat } from '@/shared/hooks/useDatePickerFormat';
 import { useTranslation } from '@/shared/hooks/useTranslation';
 import { Typography } from '@/shared/ui/typography';
 import { clsx } from 'clsx';
 import Link from 'next/link';
+import InputIcon from 'react-multi-date-picker/components/input_icon';
 
 /*import InputIcon from 'react-multi-date-picker/components/input_icon';*/
 import 'react-multi-date-picker/styles/backgrounds/bg-dark.css';
 
 import s from 'components/datePicker/datePicker.module.scss';
 
-type DatePickerContainerProps = ComponentPropsWithoutRef<typeof DatePicker> & {
-  error?: string;
+export type DatePickerContainerProps = ComponentPropsWithoutRef<typeof DatePicker> & {
+  calendarError?: string;
+  inputError?: string;
   label?: string;
 };
 
-export const DatePickerContainer: FC<DatePickerContainerProps> = ({ error, label, ...props }) => {
+export const DatePickerContainer: FC<DatePickerContainerProps> = ({
+  calendarError,
+  children,
+  inputError,
+  label,
+  onPositionChange,
+  ...props
+}) => {
   const { t } = useTranslation();
-  const { localeFormat } = useDatePickerFormat();
+  const [calendarErrorPos, setCalendarErrorPos] = useState<'bottom' | 'top'>('top');
 
-  console.log(error);
+  const mapWeekends = ({ date }: { date: DateObject }) => {
+    const isWeekend = [0, 6].includes(date.weekDay.index);
+
+    return { className: isWeekend ? 'highlight highlight-red' : '' };
+  };
+
+  const handlePositionChange: DatePickerContainerProps['onPositionChange'] = data => {
+    const newPos = data.popper.top < data.element.top ? 'bottom' : 'top';
+
+    if (newPos !== calendarErrorPos) {
+      setCalendarErrorPos(newPos);
+    }
+    onPositionChange?.(data);
+  };
+
+  const isAnyError = calendarError || inputError;
 
   return (
-    <div className={clsx(s.datePickerContainer, error && s.error)}>
+    <div className={clsx(s.datePickerContainer, isAnyError && s.error)}>
       <Typography.Regular14 className={s.label} color={'var(--color-light-900)'}>
         {label}
       </Typography.Regular14>
@@ -33,7 +57,6 @@ export const DatePickerContainer: FC<DatePickerContainerProps> = ({ error, label
         className={'bg-dark'}
         containerClassName={s.cont}
         dateSeparator={' - '}
-        format={localeFormat}
         headerOrder={['MONTH_YEAR', 'LEFT_BUTTON', 'RIGHT_BUTTON']}
         locale={t.datePicker.locale}
         mapDays={({ date }) => {
@@ -46,13 +69,29 @@ export const DatePickerContainer: FC<DatePickerContainerProps> = ({ error, label
 
           return props;
         }}
+        mapDays={mapWeekends}
+        onPositionChange={handlePositionChange}
+        render={<InputIcon />}
         weekStartDayIndex={1}
       />
+        {...props}
+      >
+        {children}
+        {calendarError && (
+          <Typography.Regular12
+            className={clsx(s.formatError, s.errorMessage)}
+            component={'p'}
+            style={{ ...(calendarErrorPos === 'top' && { order: -1 }) }}
+          >
+            {calendarError}
+          </Typography.Regular12>
+        )}
+      </DatePicker>
+
       {/*TODO: extract error message to the profile-form level*/}
-      {error && (
-        <Typography.SmallLink className={s.errorMessage}>
-          {error}
+      {inputError && (
         <Typography.Regular12 className={s.errorMessage}>
+          {inputError}
           <Link href={'/privacy-policy'} style={{ textDecoration: 'underLine' }}>
             {t.navbar.privacyPolicy}
           </Link>
