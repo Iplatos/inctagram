820bae056862496ff55c9257c53b11a9847874c0
ArtyomSamsonchik <colpachek@gmail.com>
2024-02-06 00:38:10 +0300

    feat: add transformTaggedString helper
    Refactor Trans common component to use the new transformTaggedString helper.

--- widgets/Trans/Trans.tsx
+++ widgets/Trans/Trans.tsx
@@ -1,39 +1,12 @@
-import { FC, Fragment } from 'react';
+import { FC, ReactNode } from 'react';
 
-const tagsRegex = /(<\d+>[^<>]*<\/\d+>)/;
-const openCloseTagRegex = /<(\d+)>([^<>]*)<\/(\d+)>/;
+import { transformTaggedString } from '@/shared/helpers/transformTaggedString';
 
 type TransType = {
-  tags?: Record<string, (str: string) => JSX.Element>;
+  tags: Record<string, ({ content }: { content: string }) => ReactNode>;
   text: string;
 };
 
-export const Trans: FC<TransType> = props => {
-  return <>{interpolateTags(props)}</>;
-};
-
-const interpolateTags = (data: TransType) => {
-  const { tags, text } = data;
-
-  if (!tags) {
-    return text;
-  }
-
-  const tokens = text.split(tagsRegex);
-
-  return tokens.map(token => {
-    const matchResult = openCloseTagRegex.exec(token);
-
-    if (!matchResult) {
-      return token;
-    }
-
-    const [, openTag, content, closeTag] = matchResult;
-
-    if (!openTag || !closeTag || openTag !== closeTag) {
-      return token;
-    }
-
-    return <Fragment key={content}>{tags[openTag]?.(content ?? '')}</Fragment>;
-  });
+export const Trans: FC<TransType> = ({ tags, text }) => {
+  return <>{transformTaggedString.map(text, tags)}</>;
 };
