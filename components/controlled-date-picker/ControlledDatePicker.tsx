2fc3d665523e5175437df939ceaf0a9d232c3770
ArtyomSamsonchik <colpachek@gmail.com>
2024-02-12 02:49:18 +0300

    feat: adjust ControlledDatePicker validation logic

--- components/controlled-date-picker/ControlledDatePicker.tsx
+++ components/controlled-date-picker/ControlledDatePicker.tsx
@@ -27,28 +27,66 @@ export const ControlledDatePicker = <TFieldValues extends FieldValues = FieldVal
   } = useTranslation();
 
   const {
-    field: { onBlur, onChange, value },
+    field,
     fieldState: { error },
   } = useController({ control, disabled, name });
 
-  const { stringDateFormat } = useDateFormat();
+  const { stringDateFormat, tokensSeparator } = useDateFormat();
   const [calendarError, setCalendarError] = useState<string | undefined>(undefined);
 
   const dateFormatError = interpolate(t.dateOfBirth.errors.invalidDateFormat, {
     format: () => stringDateFormat,
   });
 
-  const handleChange = (
-    date: DateObject | DateObject[] | null,
-    { isTyping }: { isTyping: boolean }
-  ) => {
-    if (date === null && isTyping) {
-      setCalendarError(dateFormatError);
-    } else {
-      setCalendarError(undefined);
+  /*DatePicker has a built-in date parsing and validation mechanism.
+    If with a valid 'date' parameter (date !== null) we try to exit the 'onChange' handler early,
+    preventing the DatePicker's internal/global state from changing, the date in the input will change anyway,
+    even though the state remains unchanged.
+    That's why I correct the value of the input only in cases where there is no discrepancy
+    between the value of the input and the DatePicker state.
+    In other cases I use the 'dateFormatError' approach.*/
+  const handleChange: DatePickerProps['onChange'] = (date, { input, isTyping }) => {
+    // The user selects a date from the calendar and no cancellation or value validation is required
+    if (!isTyping) {
+      field.onChange(date instanceof DateObject ? date.toDate() : null);
+
+      return;
     }
 
-    onChange(date instanceof DateObject ? date.toDate() : null);
+    const tokens = (input?.value ?? '').split(tokensSeparator);
+
+    // Cancel user input data if necessary and exit
+    if (inputValueShouldBeCanceled(tokens)) {
+      return false;
+    }
+
+    const isDateFormatError = checkDateFormatError(tokens);
+
+    if (isDateFormatError) {
+      setCalendarError(dateFormatError);
+      field.onChange(null);
+
+      return;
+    }
+
+    setCalendarError(undefined);
+    field.onChange(date instanceof DateObject ? date.toDate() : null);
+
+    function inputValueShouldBeCanceled(dateTokens: string[]) {
+      if (dateTokens.some(val => val.startsWith('00'))) {
+        return true;
+      }
+
+      return dateTokens.map(Number).some(num => num < 0);
+    }
+
+    function checkDateFormatError(dateTokens: string[]) {
+      if (dateTokens.length !== 3) {
+        return true;
+      }
+
+      return dateTokens.map(t => (t.length ? Number(t) : NaN)).some(isNaN);
+    }
   };
 
   const handleClose = () => {
@@ -56,7 +94,7 @@ export const ControlledDatePicker = <TFieldValues extends FieldValues = FieldVal
       setCalendarError(undefined);
       resetField(name);
     }
-    onBlur();
+    field.onBlur();
   };
 
   return (
@@ -68,7 +106,7 @@ export const ControlledDatePicker = <TFieldValues extends FieldValues = FieldVal
       locale={commonT.datePicker.locale}
       onChange={handleChange}
       onClose={handleClose}
-      value={value}
+      value={field.value}
       {...props}
     />
   );
