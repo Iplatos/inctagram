e22461c0c8caa923e2e821f3a06182bcec972042
ArtyomSamsonchik <colpachek@gmail.com>
2024-04-11 05:55:02 +0300

    feat: add `CroppedImage` common component

---
+++ shared/ui/croppedImage/CroppedImage.tsx
@@ -0,0 +1,159 @@
+import React, { CSSProperties, FC, useMemo } from 'react';
+
+import { resolveImageSrcToString } from '@/shared/helpers/resolveImageSrcToString';
+import { clsx } from 'clsx';
+import { StaticImport } from 'next/dist/shared/lib/get-img-props';
+import Image, { ImageProps } from 'next/image';
+import { z } from 'zod';
+
+import s from './croppedImage.module.scss';
+
+const DEFAULT_CROP_PROPS: CropProps = {
+  offsetX: 0.5,
+  offsetY: 0.5,
+  scale: 1,
+} as const;
+
+type CroppedImageSlot = 'image' | 'viewBox';
+type CroppedImageClasses = { [P in CroppedImageSlot]?: string };
+
+// TODO: move the validation schema and `cropProps` type to separate files which can be used as a single source of truth
+
+/**
+ * Parameters used to crop an image inside its view box.
+ *
+ * @property `offsetX` - image offset relative to the _left_ side of its view box in the range from `0` to `1` of its width.
+ * @property `offsetY` - image offset relative to the _top_ side of its view box in the range from `0` to `1` of its height.
+ * @property `scale` - image size scale relative to its view box size in range from `1` to `2`, which defines both width and height.
+ * */
+export type CropProps = {
+  offsetX: number;
+  offsetY: number;
+  scale: number;
+};
+
+export type CroppedImageProps = ImageProps & {
+  classes?: CroppedImageClasses;
+} & Partial<CropProps>;
+
+export const CroppedImage: FC<CroppedImageProps> = ({
+  classes = {},
+  fill,
+  height,
+  offsetX = DEFAULT_CROP_PROPS.offsetX,
+  offsetY = DEFAULT_CROP_PROPS.offsetY,
+  scale = DEFAULT_CROP_PROPS.scale,
+  style,
+  width,
+  ...props
+}) => {
+  const { cropProps: finalCropProps, errors } = useCropPropsValidation(offsetX, offsetY, scale);
+  const imageSizeIsSetExplicitly = Boolean(fill || (width && height));
+
+  ({ offsetX, offsetY, scale } = imageSizeIsSetExplicitly ? finalCropProps : DEFAULT_CROP_PROPS);
+
+  if (!imageSizeIsSetExplicitly) {
+    console.warn(getCropPropsError('implicitSize', props.src, errors));
+  } else if (errors.length) {
+    console.error(getCropPropsError('invalidCropProps', props.src, errors));
+  }
+
+  // Using the `style` instead of `className` ensures that the necessary CSS properties are not overwritten
+  const viewBoxStyle: CSSProperties = {
+    overflow: 'hidden',
+    ...(fill ? { height: '100%', position: 'absolute', width: '100%' } : { height, width }),
+  };
+
+  // Once the scaling box has been scaled, we need to move it back by the scale value
+  // to return the picture to its original position.
+  // Since the offset is applied to the enlarged scaling box,
+  // we need to convert the size increment of the original scaling box to the size increment of the enlarged box.
+  const getScalingBoxOffset = (pos: number, scale: number) => (pos * scale - pos) / scale;
+  const toCSSPercentage = (...args: number[]) => args.map(arg => arg * 100 + '%').join(' ');
+
+  const scalingBoxOffsetX = getScalingBoxOffset(offsetX, scale) * -1;
+  const scalingBoxOffsetY = getScalingBoxOffset(offsetY, scale) * -1;
+  const scalingBoxStyle: CSSProperties = {
+    height: toCSSPercentage(scale),
+    position: 'relative',
+    translate: toCSSPercentage(scalingBoxOffsetX, scalingBoxOffsetY),
+    width: toCSSPercentage(scale),
+  };
+
+  const imageStyle: CSSProperties = {
+    ...style,
+    objectPosition: toCSSPercentage(offsetX, offsetY),
+  };
+
+  const cls = getClassNames(classes);
+  const scaleImageSize = (size: `${number}` | number | undefined) =>
+    size ? Number(size) * scale : size;
+
+  return (
+    <div className={cls.viewBox} style={viewBoxStyle}>
+      <div className={s.scalingBox} style={scalingBoxStyle}>
+        <Image
+          className={cls.image}
+          fill={fill}
+          height={scaleImageSize(height)}
+          style={imageStyle}
+          width={scaleImageSize(width)}
+          {...props}
+        />
+      </div>
+    </div>
+  );
+};
+
+const getClassNames = (classes: CroppedImageClasses): CroppedImageClasses => ({
+  image: clsx(s.image, classes.image),
+  viewBox: clsx(s.viewBox, classes.viewBox),
+});
+
+type ErrorReason = 'implicitSize' | 'invalidCropProps';
+const getCropPropsError = (
+  reason: ErrorReason,
+  src: StaticImport | string,
+  errors: z.ZodIssue[]
+) => {
+  // prettier-ignore
+  const errorsMap: Record<ErrorReason, string> = {
+    implicitSize: `CroppedImage with src "${resolveImageSrcToString(src)}" must have either a "fill" prop or both "width" and "height" props for cropping props to have an effect. Consider using the "Next/Image" component if you need the image to be sized to the size of its replaced content.`,
+    invalidCropProps: [
+      `CroppedImage with src "${resolveImageSrcToString(src)}" received wrong cropping props.`,
+      ...errors.map(i => i.message),
+      'Default cropping props have been restored.',
+    ].join(' '),
+  };
+
+  return errorsMap[reason];
+};
+
+const useCropPropsValidation = (offsetX: number, offsetY: number, scale: number) =>
+  useMemo((): { cropProps: CropProps; errors: z.ZodIssue[] } => {
+    const getCommonNumberSchema = (value: number, min: number, max: number, field: string) => {
+      const message = `"${field}" prop value should be between "${min}" and "${max}", received value is "${value}".`;
+
+      return z.number().min(min, message).max(max, message);
+    };
+
+    const schema = z.object({
+      offsetX: getCommonNumberSchema(offsetX, 0, 1, 'offsetX'),
+      offsetY: getCommonNumberSchema(offsetY, 0, 1, 'offsetY'),
+      scale: getCommonNumberSchema(scale, 1, 2, 'scale'),
+    } satisfies Record<keyof CropProps, z.ZodTypeAny>);
+
+    const result = schema.safeParse({ offsetX, offsetY, scale });
+
+    if (!result.success) {
+      return {
+        cropProps: DEFAULT_CROP_PROPS,
+        errors: result.error.issues,
+      };
+    }
+
+    return {
+      cropProps: { offsetX, offsetY, scale },
+      errors: [],
+    };
+  }, [offsetX, offsetY, scale]);
---
+++ shared/ui/croppedImage/croppedImage.stories.tsx
@@ -0,0 +1,127 @@
+import MockUserAvatar from '@/assets/img/mock-user-avatar.jpg';
+import { Typography } from '@/shared/ui';
+import { Meta, StoryObj } from '@storybook/react';
+
+import { CroppedImage } from './CroppedImage';
+
+/**
+ * This component is based on the Next.js [Image](https://nextjs.org/docs/pages/api-reference/components/image) component with the additional ability to scale and crop the image.
+ * All these manipulations are done by CSS. The component supports all the features of Next/Image component.
+ *
+ * __Important__: for the cropping and scaling logic to work correctly, the component needs to be explicitly dimensioned.
+ * This can be done either by passing `fill={true}` or by specifying values for `width` and `height` props at the same time.
+ * [Read more](https://nextjs.org/docs/pages/api-reference/components/image#fill) about the `fill` prop.
+ * */
+const meta = {
+  argTypes: {
+    fill: {
+      control: { type: 'boolean' },
+      description:
+        'Has [the same](https://nextjs.org/docs/pages/api-reference/components/image#fill) behaviour as Next.js `fill` prop.',
+      table: {
+        type: { summary: 'boolean | undefined' },
+      },
+    },
+
+    height: {
+      control: 'number',
+      description: `Has [the same](https://nextjs.org/docs/pages/api-reference/components/image#height) behaviour as Next.js \`height\` prop.`,
+      if: { arg: 'fill', truthy: false },
+      table: { type: { summary: '`${sting}` | number | undefined' } },
+    },
+
+    offsetX: {
+      control: { max: 1, min: 0, step: 0.01, type: 'range' },
+      description: `Image offset relative to the _left_ side of its view box in the range from \`0\` to \`1\` of its width.\t
+          Allowed value is between \`0\` and \`1\`.\t
+          If the allowed value is not met, all cropping and scaling props will be returned to the default value.
+          Also an error will be shown in the console.`,
+      table: {
+        defaultValue: { summary: 0.5 },
+      },
+    },
+
+    offsetY: {
+      control: { max: 1, min: 0, step: 0.01, type: 'range' },
+      description: `Image offset relative to the _top_ side of its view box in the range from \`0\` to \`1\` of its height.\t
+          Allowed value is between \`0\` and \`1\`.\t
+          If the allowed value is not met, all cropping and scaling props will be returned to the default value.
+          Also an error will be shown in the console.`,
+      table: {
+        defaultValue: { summary: 0.5 },
+      },
+    },
+
+    scale: {
+      control: { max: 2, min: 1, step: 0.1, type: 'range' },
+      description: `Image size scale relative to its view box size in range from \`1\` to \`2\`, which defines both width and height\t
+          .Allowed value is between \`1\` and \`2\`.\t
+          If the allowed value is not met, all cropping and scaling props will be returned to the default value.
+          Also an error will be shown in the console.`,
+      table: {
+        defaultValue: { summary: 1 },
+      },
+    },
+    width: {
+      control: 'number',
+      description: `Has [the same](https://nextjs.org/docs/pages/api-reference/components/image#width) behaviour as Next.js \`width\` prop.`,
+      if: { arg: 'fill', truthy: false },
+      table: { type: { summary: '`${sting}` | number | undefined' } },
+    },
+  },
+
+  component: CroppedImage,
+  decorators: [
+    (Story, ctx) => (
+      <div>
+        <div
+          style={{
+            ...(ctx.args.fill && { aspectRatio: 1, maxWidth: '300px', position: 'relative' }),
+          }}
+        >
+          <Story />
+        </div>
+        <Typography.Regular16 style={{ display: 'inline-block', marginTop: '1rem' }}>
+          <Typography.Bold16>TIP: </Typography.Bold16>
+          Open the devtools console and see the errors and default actions associated with the wrong
+          cropping props.
+        </Typography.Regular16>
+      </div>
+    ),
+  ],
+  parameters: { controls: { include: ['offsetX', 'offsetY', 'scale', 'fill', 'width', 'height'] } },
+  tags: ['autodocs'],
+  title: 'UI/CroppedImage',
+} satisfies Meta<typeof CroppedImage>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    alt: '',
+    fill: true,
+    offsetX: 0.5,
+    offsetY: 0.5,
+    scale: 1,
+    src: MockUserAvatar,
+  },
+};
+
+export const WidenedOffsetRanges: Story = {
+  argTypes: {
+    offsetX: { control: { max: 2, min: -1 } },
+    offsetY: { control: { max: 2, min: -1 } },
+    scale: { control: { max: 3, min: 0 } },
+  },
+  args: Primary.args,
+};
+
+export const FixedAspectRatio: Story = {
+  args: {
+    ...Primary.args,
+    fill: false,
+    height: 200,
+    width: 320,
+  },
+};
