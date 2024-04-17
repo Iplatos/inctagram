f16d8540f26055f9c7e0e9b22626f7b6d0aa72bb
ArtyomSamsonchik <colpachek@gmail.com>
2024-04-17 04:12:44 +0300

    refactor: `Avatar` component.
    Implemented cropping props support.
    Removed Radix related components.
    Supplemented storybook docs.

--- shared/ui/avatar/avatar.stories.tsx
+++ shared/ui/avatar/avatar.stories.tsx
@@ -1,45 +1,20 @@
 import { useEffect, useState } from 'react';
 
-import { AvatarFallback } from '@/assets/icons/avatar-fallback';
 import UserAvatar from '@/assets/img/mock-user-avatar.jpg';
+import { Typography } from '@/shared/ui';
+import * as CroppedImageStories from '@/shared/ui/croppedImage/croppedImage.stories';
 import { Meta, StoryObj } from '@storybook/react';
 
-import { Avatar, AvatarProps } from './avatar';
+import { Avatar, AvatarClasses, AvatarProps } from './avatar';
 
-type AvatarPropsAndCustomArgs = AvatarProps & { delayMs?: number; imageType: 'dynamic' | 'static' };
-
-const meta = {
-  argTypes: {
-    delayMs: {
-      control: 'number',
-      if: { arg: 'imageType', eq: 'dynamic' },
-    },
-    imageType: {
-      control: 'inline-radio',
-      defaultValue: 'static',
-      options: ['static', 'dynamic'],
-    },
-  },
-  component: Avatar,
-
-  parameters: {
-    controls: {
-      include: ['imageType', 'delayMs', 'alt', 'size', 'fallbackDelayMs'],
-    },
-  },
-  tags: ['autodocs'],
-  title: 'UI/Avatar',
-} satisfies Meta<AvatarPropsAndCustomArgs>;
-
-export default meta;
-type Story = StoryObj<AvatarPropsAndCustomArgs>;
-
-const wait = (ms?: number) =>
-  new Promise(res => {
-    setTimeout(res, ms);
-  });
+type AvatarPropsAndCustomArgs = AvatarProps & { imageType: 'dynamic' | 'static' };
 
 const CustomRender = ({ delayMs, imageType, src, ...args }: AvatarPropsAndCustomArgs) => {
+  const wait = (ms?: number) =>
+    new Promise(res => {
+      setTimeout(res, ms);
+    });
+
   const [url, setUrl] = useState('');
 
   useEffect(() => {
@@ -53,28 +28,151 @@ const CustomRender = ({ delayMs, imageType, src, ...args }: AvatarPropsAndCustom
       .then(json => setUrl(json[0].url));
   }, [delayMs, imageType]);
 
-  return <Avatar fill src={imageType === 'static' ? src : url} {...args} />;
+  return (
+    <Avatar
+      delayMs={delayMs ? delayMs + 500 : delayMs}
+      src={imageType === 'static' ? src : url}
+      {...args}
+    />
+  );
 };
+
+/**
+ * An image element with a fallback for representing the user. It is build on top of the [CroppedImage](?path=/docs/ui-croppedimage--docs) component. All features of the Next.js [Image](https://nextjs.org/docs/pages/api-reference/components/image) component are supported.
+ * The inner CroppedImage is rendered using [fill](https://nextjs.org/docs/pages/api-reference/components/image#fill) prop, so `fill`, `height`, `width` props are omitted.
+ *
+ * Main features:
+ * * Fallback is shown if no image `src` is specified or the image was loaded with an error.
+ * * The display of the fallback can be delayed, for example if `src` is not available immediately after Avatar mount.
+ * */
+const meta = {
+  argTypes: {
+    classes: {
+      control: { type: 'object' },
+      description: `An object containing the names of the classes corresponding to the
+        component slots. Provided classnames will be merged with default slots classnames.`,
+      table: {
+        defaultValue: { summary: '{ }' },
+        type: {
+          //prettier-ignore
+          detail: `Partial<${JSON.stringify(
+            { avatarRoot: 'string', fallback: 'string', image: 'string', viewBox: 'string' } as AvatarClasses,
+            null,
+            2
+          )}>`,
+          summary: 'AvatarClasses',
+        },
+      },
+    },
+
+    delayMs: {
+      control: 'number',
+      description:
+        'Can be used to delay fallback rendering if image `src` is not available immediately after Avatar is mounted',
+    },
+
+    fallback: {
+      defaultValue: { summary: '<AvatarFallback />' },
+      description: `An element that renders when the image hasn't loaded. This means whilst it's loading, or if there was an error.
+        If you notice a flash during loading, you can provide a \`delayMs\` prop to delay its rendering.`,
+      table: { type: { summary: 'ReactNode' } },
+    },
+
+    imageType: {
+      control: 'inline-radio',
+      defaultValue: { summary: 'static' },
+      description:
+        '_STORYBOOK_SPECIFIC_SETTING:_ controls whether to use a static image from the assets folder or get the image from the [jsonplaceholder fake API](https://jsonplaceholder.typicode.com/).',
+      options: ['static', 'dynamic'],
+    },
+
+    offsetX: CroppedImageStories.default.argTypes.offsetX,
+
+    offsetY: CroppedImageStories.default.argTypes.offsetY,
+
+    scale: CroppedImageStories.default.argTypes.scale,
+
+    size: {
+      control: 'radio',
+      description: `A preset set of common avatar sizes. All sizes are set with CSS em &lt;length&gt; value`,
+      options: ['small', 'medium', 'large'],
+      table: {
+        defaultValue: { summary: 'medium' },
+        type: { summary: 'small | medium | large' },
+      },
+    },
+
+    src: {
+      control: false,
+      description: 'Unlike Next.js Image it is not required.',
+      table: { type: { summary: 'string | StaticImport' } },
+    },
+  },
+
+  component: CustomRender,
+
+  decorators: [
+    Story => (
+      <div style={{ fontSize: '3rem' }}>
+        <Typography.Regular16 component={'p'} style={{ marginBottom: '1rem' }}>
+          The story&apos;s font size has been changed to <em>3 rem</em> to enlarge the content
+        </Typography.Regular16>
+        <Story />
+      </div>
+    ),
+  ],
+  parameters: {
+    controls: {
+      include: [
+        'classes',
+        'delayMs',
+        'fallback',
+        'imageType',
+        'offsetX',
+        'offsetY',
+        'scale',
+        'size',
+        'src',
+      ] satisfies (keyof AvatarPropsAndCustomArgs)[],
+    },
+  },
+  title: 'UI/Avatar',
+} satisfies Meta<AvatarPropsAndCustomArgs>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
 
 export const StaticImage: Story = {
   args: {
-    fallback: <AvatarFallback />,
+    ...CroppedImageStories.Primary.args,
     imageType: 'static',
     src: UserAvatar,
   },
-  render: CustomRender,
 };
 
 export const Fallback: Story = {
   args: {
     ...StaticImage.args,
+    src: undefined,
+  },
+};
+
+export const CustomFallback: Story = {
+  args: {
+    ...Fallback.args,
+    fallback: <Typography.Bold16 style={{ fontSize: 'inherit' }}>GG</Typography.Bold16>,
+  },
+};
+
+export const WrongUrl: Story = {
+  args: {
+    ...StaticImage.args,
     src: 'does-not-exist',
   },
-  render: CustomRender,
 };
 
 export const AsLink: Story = {
-  args: StaticImage.args,
+  args: { ...StaticImage.args, alt: 'To alert docs' },
   render: args => (
     <a href={'/?path=/docs/ui-alert--docs'} title={'go to the Alert docs'}>
       <CustomRender {...args} />
@@ -84,5 +182,4 @@ export const AsLink: Story = {
 
 export const RemoteImage: Story = {
   args: { ...StaticImage.args, delayMs: 3000, imageType: 'dynamic' },
-  render: CustomRender,
 };
--- shared/ui/avatar/avatar.tsx
+++ shared/ui/avatar/avatar.tsx
@@ -1,53 +1,96 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactEventHandler, ReactNode, useEffect, useRef, useState } from 'react';
 
+import { AvatarFallback } from '@/assets/icons/avatar-fallback';
 import { capitalise } from '@/shared/helpers/capitalise';
 import { resolveImageSrcToString } from '@/shared/helpers/resolveImageSrcToString';
-import * as AvatarRadix from '@radix-ui/react-avatar';
+import { CroppedImage, CroppedImageProps, CroppedImageSlot } from '@/shared/ui/croppedImage';
 import { clsx } from 'clsx';
 import { StaticImport } from 'next/dist/shared/lib/get-img-props';
-import Image, { ImageProps as NextImageProps } from 'next/image';
 
 import s from './avatar.module.scss';
 
-type AvatarSlot = 'avatarRoot' | 'fallback' | 'image';
+export type AvatarSlot = 'avatarRoot' | 'fallback' | Extract<CroppedImageSlot, 'image'>;
 export type AvatarClasses = { [P in AvatarSlot]?: string };
 type AvatarSize = 'large' | 'medium' | 'small';
+type AvatarStatus = 'error' | 'pending' | 'success';
 
 type OwnProps = {
   alt?: string;
   classes?: AvatarClasses;
+  delayMs?: number;
   fallback?: ReactNode;
-  fallbackDelayMs?: number;
   size?: AvatarSize;
   src?: StaticImport | string;
 };
 
-export type AvatarProps = OwnProps & Omit<NextImageProps, 'className' | keyof OwnProps>;
+export type AvatarProps = OwnProps &
+  Omit<CroppedImageProps, 'fill' | 'height' | 'width' | keyof OwnProps>;
 
-export const Avatar: FC<AvatarProps> = ({
-  alt = 'user avatar',
+export const Avatar: FC<AvatarProps> = ({ src, ...props }) => (
+  <InnerComponent key={resolveImageSrcToString(src)} src={src} {...props} />
+);
+
+const InnerComponent: FC<AvatarProps> = ({
+  alt = '',
   classes = {},
-  fallback,
-  fallbackDelayMs,
+  delayMs,
+  fallback = <AvatarFallback />,
+  onError,
+  onLoad,
   size = 'medium',
   src,
   ...props
 }) => {
+  const [status, setStatus] = useState<AvatarStatus>('pending');
+  const [delayExpired, setDelayExpired] = useState(!delayMs);
+  const timerRef = useRef<number>();
+
+  useEffect(() => {
+    if (delayMs && status === 'pending') {
+      timerRef.current = window.setTimeout(() => {
+        setDelayExpired(true);
+      }, delayMs);
+    }
+
+    return () => {
+      clearTimeout(timerRef.current);
+    };
+  }, [delayMs, status]);
+
   const cls = getClassNames(classes, size);
 
+  const handleLoad: ReactEventHandler<HTMLImageElement> = e => {
+    setStatus('success');
+    onLoad?.(e);
+  };
+  const handleError: ReactEventHandler<HTMLImageElement> = e => {
+    setStatus('error');
+    onError?.(e);
+  };
+
+  const showAvatar = src && status !== 'error';
+  const showFallback = status === 'error' || delayExpired;
+
   return (
-    <AvatarRadix.Root className={cls.avatarRoot}>
-      <AvatarRadix.Image alt={alt} asChild src={resolveImageSrcToString(src)}>
-        {src && <Image alt={alt} className={cls.image} src={src} {...props} />}
-      </AvatarRadix.Image>
-      <AvatarRadix.Fallback className={cls.fallback} delayMs={fallbackDelayMs}>
-        {fallback}
-      </AvatarRadix.Fallback>
-    </AvatarRadix.Root>
+    <span className={cls.avatarRoot}>
+      {showAvatar ? (
+        <CroppedImage
+          alt={alt}
+          classes={cls}
+          fill
+          onError={handleError}
+          onLoad={handleLoad}
+          src={src}
+          {...props}
+        />
+      ) : (
+        <span className={cls.fallback}>{showFallback && fallback}</span>
+      )}
+    </span>
   );
 };
 
-const getClassNames = (classes: AvatarClasses, size: AvatarSize): AvatarClasses => ({
+const getClassNames = (classes: AvatarClasses, size: AvatarSize): Required<AvatarClasses> => ({
   avatarRoot: clsx(s[`avatarRoot${capitalise(size)}`], classes.avatarRoot),
   fallback: clsx(s.fallback, classes.fallback),
   image: clsx(s.image, classes.image),
