475c679e5ee55c843e5f72d8f21aec52506e8322
iplatos <platonoff9168@gmail.com>
2024-04-25 13:32:27 +0300

    fix:refactor concatString

--- shared/helpers/concatString.ts
+++ shared/helpers/concatString.ts
@@ -1,3 +1,16 @@
 export const concatString = (string: string) => {
-  return string.toLowerCase().split(' ').join('');
+  const splitted = string.split(' ');
+
+  if (splitted.length > 1) {
+    const array = string
+      .trim()
+      .split(/\s+/)
+      .map(word => word[0].toUpperCase() + word.substring(1));
+
+    array[0] = splitted[0];
+
+    return array.join('');
+  }
+
+  return string.toLowerCase();
 };
