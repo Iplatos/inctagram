fabc6b98f0f3d826aeae420f492c7956dd453b6a
ArtyomSamsonchik <colpachek@gmail.com>
2024-05-17 02:15:10 +0300

    feat: add logic for uploading user avatar

--- features/accounts/edit/add-profile-photo/add-profile-photo.tsx
+++ features/accounts/edit/add-profile-photo/add-profile-photo.tsx
@@ -1,26 +1,68 @@
+import { useState } from 'react';
+
+import { AvatarUploader } from '@/features/avatar-uploader';
+import {
+  useLazyMyAvatarBase64Query,
+  useMeQuery,
+  useSetAvatarMutation,
+} from '@/shared/api/users-api';
+import { getDefaultCropProps } from '@/shared/helpers/getDefaultCropProps';
 import { useTranslation } from '@/shared/hooks/useTranslation';
-import { Button } from '@/shared/ui';
-import Image from 'next/image';
+import { Avatar, Button } from '@/shared/ui';
+import { CropProps } from '@/shared/ui/croppedImage';
 
 import style from './add-profile-photo.module.scss';
 
-import AccountPhotoImage from '@/assets/icons/avatar-fallback.svg?url';
-
 export const AddProfilePhoto = () => {
   const { addProfilePhoto: t } = useTranslation().t.generalInformation;
+  const [open, setOpen] = useState(false);
 
-  function addProfilePhoto() {
-    //open modal upload photo
-  }
+  const { data: meResponse } = useMeQuery();
+  const [getAvatarBase64, { data: avatarBase64 }] = useLazyMyAvatarBase64Query();
+  const [uploadAvatar] = useSetAvatarMutation();
+
+  const handleAvatarUpload = (image: Blob, { offsetX, offsetY, scale }: CropProps) => {
+    const formData = new FormData();
+
+    formData.append('offsetX', offsetX.toString());
+    formData.append('offsetY', offsetY.toString());
+    formData.append('scale', scale.toString());
+    formData.append('file', image, 'user-avatar`');
+
+    uploadAvatar(formData);
+  };
+
+  // Manual destructuring to prevent unrecognized props from backend such as `updatedAt` from being passed to the internal `img` component.
+  const dCP = getDefaultCropProps();
+  const {
+    offsetX = dCP.offsetX,
+    offsetY = dCP.offsetY,
+    scale = dCP.scale,
+    url: src,
+  } = meResponse?.data.avatar ?? {};
 
   return (
     <div className={style.photoContainer}>
-      <div className={style.imageBlock}>
-        <Image alt={'profile photo'} src={AccountPhotoImage} />
-      </div>
-      <Button onClick={addProfilePhoto} variant={'tertiary'}>
+      <Avatar
+        classes={{ avatarRoot: style.avatar }}
+        priority
+        {...{ offsetX, offsetY, scale, src }}
+      />
+      <Button
+        onClick={() => setOpen(true)}
+        onMouseEnter={() => getAvatarBase64(undefined, true)}
+        variant={'tertiary'}
+      >
         {t.submitButton}
       </Button>
+
+      <AvatarUploader
+        avatar={avatarBase64 ?? undefined}
+        initCropProps={{ offsetX, offsetY, scale }}
+        onClose={() => setOpen(false)}
+        onImageSave={handleAvatarUpload}
+        open={open}
+      />
     </div>
   );
 };
--- widgets/accounts/edit/edit-profile.tsx
+++ widgets/accounts/edit/edit-profile.tsx
@@ -8,37 +8,30 @@ import {
 } from '@/entities/accounts/edit';
 import { TabContent, Tabs } from '@/shared/ui';
 
-import style from './edit-profile.module.scss';
-
 export const EditProfile = () => {
   return (
-    <div className={style.profileContainer}>
-      <Tabs
-        defaultValue={'general information'}
-        fullWidth
-        tabs={[
-          {
-            title: 'General information',
-            value: 'general information',
-          },
-          { title: 'Devices', value: 'devices' },
-          { title: 'Account Management', value: 'account management' },
-          { title: 'My payments', value: 'my payments' },
-        ]}
-      >
-        <TabContent value={'general information'}>
-          <GeneralInformation />
-        </TabContent>
-        <TabContent value={'devices'}>
-          <Devices />
-        </TabContent>
-        <TabContent value={'account management'}>
-          <AccountManagement />
-        </TabContent>
-        <TabContent value={'my payments'}>
-          <MyPayments />
-        </TabContent>
-      </Tabs>
-    </div>
+    <Tabs
+      defaultValue={'general information'}
+      fullWidth
+      tabs={[
+        { title: 'General information', value: 'general information' },
+        { title: 'Devices', value: 'devices' },
+        { title: 'Account Management', value: 'account management' },
+        { title: 'My payments', value: 'my payments' },
+      ]}
+    >
+      <TabContent value={'general information'}>
+        <GeneralInformation />
+      </TabContent>
+      <TabContent value={'devices'}>
+        <Devices />
+      </TabContent>
+      <TabContent value={'account management'}>
+        <AccountManagement />
+      </TabContent>
+      <TabContent value={'my payments'}>
+        <MyPayments />
+      </TabContent>
+    </Tabs>
   );
 };
