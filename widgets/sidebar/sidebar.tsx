dfb1d5894ed0fdb5bb0f35d781c4b1eea5fbb652
Marina <marina1497@list.ru>
2024-09-09 15:23:02 +0300

    feat: add action to control creating posts modal into sidebar

--- widgets/sidebar/sidebar.tsx
+++ widgets/sidebar/sidebar.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 import { ConfirmModal } from '@/features/confirm-modal';
 import { useLogoutMutation, useRefreshTokenQuery } from '@/shared/api/auth-api';
+import { openModal } from '@/shared/api/modal-slice';
 import { useLazyGetMeQuery } from '@/shared/api/users-api';
 import { useTranslation } from '@/shared/hooks/useTranslation';
 import { Typography } from '@/shared/ui';
@@ -19,9 +21,13 @@ import Link from 'next/link';
 import s from './sidebar.module.scss';
 
 import { Trans } from '../Trans/Trans';
+import { ModalCreatePublication } from '../modal-create-publication';
 
 export const Sidebar = () => {
   const t = useTranslation().t.common;
+
+  const dispatch = useDispatch();
+
   const [open, setOpen] = useState(false);
 
   const [logoutTrigger, { isLoading: isLogOutLoading }] = useLogoutMutation();
@@ -49,69 +55,76 @@ export const Sidebar = () => {
   };
 
   return (
-    <div className={s.container}>
-      <div className={s.buttonsGroup}>
-        <Typography.Regular14 className={s.button} component={Link} href={'/'}>
-          <HomeOutline className={s.svg} />
-          Home
+    <>
+      <div className={s.container}>
+        <div className={s.buttonsGroup}>
+          <Typography.Regular14 className={s.button} component={Link} href={'/'}>
+            <HomeOutline className={s.svg} />
+            Home
+          </Typography.Regular14>
+
+          <Typography.Regular14
+            className={s.button}
+            onClick={() => dispatch(openModal(true))}
+            tabIndex={1}
+          >
+            <PlusSquareOutline className={s.svg} />
+            Create
+          </Typography.Regular14>
+          <Typography.Regular14 className={s.button} component={Link} href={'/my-profile'}>
+            <PersonOutline className={s.svg} />
+            My Profile
+          </Typography.Regular14>
+          <Typography.Regular14 className={s.button} tabIndex={1}>
+            <MessageCircleOutline className={s.svg} />
+            Messenger
+          </Typography.Regular14>
+          <Typography.Regular14 className={s.button} tabIndex={1}>
+            <SearchOutline className={s.svg} />
+            Search
+          </Typography.Regular14>
+        </div>
+
+        <div className={s.buttonsGroup}>
+          <Typography.Regular14 className={s.button} tabIndex={1}>
+            <TrendingUpOutline className={s.svg} />
+            Statistics
+          </Typography.Regular14>
+          <Typography.Regular14 className={s.button} tabIndex={1}>
+            <BookmarkOutline className={s.svg} />
+            Favorites
+          </Typography.Regular14>
+        </div>
+
+        <Typography.Regular14
+          className={clsx(s.button, s.buttonLogout)}
+          onClick={() => setOpen(true)}
+          tabIndex={1}
+        >
+          <LogOutOutline className={s.svg} />
+          Log Out
         </Typography.Regular14>
 
-        <Typography.Regular14 className={s.button} tabIndex={1}>
-          <PlusSquareOutline className={s.svg} />
-          Create
-        </Typography.Regular14>
-        <Typography.Regular14 className={s.button} component={Link} href={'/my-profile'}>
-          <PersonOutline className={s.svg} />
-          My Profile
-        </Typography.Regular14>
-        <Typography.Regular14 className={s.button} tabIndex={1}>
-          <MessageCircleOutline className={s.svg} />
-          Messenger
-        </Typography.Regular14>
-        <Typography.Regular14 className={s.button} tabIndex={1}>
-          <SearchOutline className={s.svg} />
-          Search
-        </Typography.Regular14>
+        <ConfirmModal
+          cancelButtonTitle={t.modal.buttonNames.cancel}
+          confirmButtonTitle={t.modal.buttonNames.confirm}
+          disabled={isLogOutLoading}
+          headerTitle={t.logOutModal.title}
+          onCancel={closeModal}
+          onConfirm={handleLogout}
+          open={open}
+        >
+          <Typography.Regular14>
+            <Trans
+              tags={{
+                email: () => <Typography.Bold16>{meResponse?.data.email}</Typography.Bold16>,
+              }}
+              text={t.logOutModal.description}
+            />
+          </Typography.Regular14>
+        </ConfirmModal>
       </div>
-
-      <div className={s.buttonsGroup}>
-        <Typography.Regular14 className={s.button} tabIndex={1}>
-          <TrendingUpOutline className={s.svg} />
-          Statistics
-        </Typography.Regular14>
-        <Typography.Regular14 className={s.button} tabIndex={1}>
-          <BookmarkOutline className={s.svg} />
-          Favorites
-        </Typography.Regular14>
-      </div>
-
-      <Typography.Regular14
-        className={clsx(s.button, s.buttonLogout)}
-        onClick={() => setOpen(true)}
-        tabIndex={1}
-      >
-        <LogOutOutline className={s.svg} />
-        Log Out
-      </Typography.Regular14>
-
-      <ConfirmModal
-        cancelButtonTitle={t.modal.buttonNames.cancel}
-        confirmButtonTitle={t.modal.buttonNames.confirm}
-        disabled={isLogOutLoading}
-        headerTitle={t.logOutModal.title}
-        onCancel={closeModal}
-        onConfirm={handleLogout}
-        open={open}
-      >
-        <Typography.Regular14>
-          <Trans
-            tags={{
-              email: () => <Typography.Bold16>{meResponse?.data.email}</Typography.Bold16>,
-            }}
-            text={t.logOutModal.description}
-          />
-        </Typography.Regular14>
-      </ConfirmModal>
-    </div>
+      <ModalCreatePublication />
+    </>
   );
 };
