5462f184dfa921c04859aa17bc4f6e342f7bd400
Marina <marina1497@list.ru>
2024-09-24 15:19:15 +0300

    init: start developing post publication

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,27 @@
 import React, { useRef, useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 import { Popover } from '@/entities/photo-slider';
 import { Crop } from '@/entities/photo-slider/crop/crop';
 import { TriggerButton } from '@/entities/photo-slider/trigger-button/trigger-button';
 import { Zoom } from '@/entities/photo-slider/zoom/zoom';
-import { DEPRECATED_Modal, PhotoGallery, PhotoSlider, SliderRef } from '@/features';
+import {
+  AddPhotoCard,
+  DEPRECATED_Modal,
+  FilterPhotoCard,
+  PhotoGallery,
+  PhotoSlider,
+  SliderRef,
+} from '@/features';
 import { useRefreshTokenQuery } from '@/shared/api/auth-api';
+import { openModal } from '@/shared/api/modal-slice';
 import { resolveImageSrcToString } from '@/shared/helpers';
 import { onCropDone } from '@/shared/helpers/onCropDone';
 import { Button } from '@/shared/ui';
 import { HeadMeta } from '@/widgets/HeadMeta/HeadMeta';
 import { getLayout } from '@/widgets/Layout/Layout';
+import { ModalCreatePublication } from '@/widgets/modal-create-publication';
+import { ModalSaveDraft } from '@/widgets/modal-save-draft/modal-save-draft';
 
 import MockImage from '../assets/img/mock-user-avatar.jpg';
 
@@ -31,6 +42,8 @@ function Home() {
   //   return <div style={{ marginLeft: '300px' }}>Unauthorized</div>;
   // }
   // const [open, setOpen] = useState(false);
+
+  const dispatch = useDispatch();
 
   const [addedImages, setAddedImages] = useState<UploadedImagesType[]>(
     // new Array(5).fill(0).map(i => resolveImageSrcToString(MockImage)) as string[]
@@ -58,16 +71,31 @@ function Home() {
       }
     `}</style>
       <HeadMeta title={'main'} />
-      {/* <div style={{ marginLeft: '300px' }}>Hello World!</div> */}
+      <div style={{ marginLeft: '300px' }}>Hello World!</div>
+      {/* <Button onClick={() => setOpen(true)}>Open Modal</Button> */}
 
-      <PhotoSlider
-        addedImages={addedImages.map(i => i.imageSrc)}
-        imgAfterCrop={imgAfterCrop}
-        setImgAfterCrop={setImgAfterCrop}
-      />
-
-      <PhotoGallery items={addedImages.map(i => i.imageSrc).map(i => ({ original: i }))} />
+      {/* <DEPRECATED_Modal className={'modalContainer'} onClose={() => setOpen(false)} open={open}> */}
+      {/* <PhotoSlider /> */}
+      {/* <PhotoGallery images={addedImages} /> */}
       {/* </DEPRECATED_Modal> */}
+
+      {/*  */}
+      {/* <button onClick={() => dispatch(openModal(true))}>!!!!click</button> */}
+
+      {/* <ModalCreatePublication /> */}
+
+      {/* <FilterPhotoCard
+        items={[
+          {
+            filter: 'normal',
+            src: 'https://images.pexels.com/photos/17756265/pexels-photo-17756265.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+          },
+          {
+            filter: 'normal',
+            src: 'https://images.pexels.com/photos/17756265/pexels-photo-17756265.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+          },
+        ]}
+      /> */}
     </>
   );
 }
--- widgets/modal-create-publication/modal-create-publication.tsx
+++ widgets/modal-create-publication/modal-create-publication.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
-import { AddPhotoCard } from '@/features';
+import { AddPhotoCard, FilterPhotoCard } from '@/features';
 import { closeModal } from '@/shared/api/modal-slice';
 import { useAppSelector } from '@/shared/api/store';
 import { useTranslation } from '@/shared/hooks';
 import { Modal, ModalCard } from '@/shared/ui';
 
 import s from './modal-create-publication.module.scss';
+
+enum PostStatus {
+  Init,
+  Cropping,
+  Filter,
+  Publication,
+}
 
 export const ModalCreatePublication = () => {
   const { t } = useTranslation();
@@ -20,16 +27,58 @@ export const ModalCreatePublication = () => {
     dispatch(closeModal(false));
   };
 
+  const [files, setFiles] = useState<File[]>([]);
+
+  const [postStatus, setPostStatus] = useState<PostStatus>(PostStatus.Init);
+
+  // console.log(files);
+
+  /* eslint-disable perfectionist/sort-objects -- preserve the natural order of post creation statuses */
+
+  const steps: Record<PostStatus, () => JSX.Element> = {
+    [PostStatus.Init]: () => (
+      <AddPhotoCard
+        error={null}
+        onClose={closeModalHandler}
+        onFileInputChange={e => {
+          if (e.target.files?.length) {
+            const file = e.target.files[0];
+
+            setFiles([...files, file]);
+            setPostStatus(PostStatus.Filter);
+          }
+        }}
+        primaryButtonTitle={t.editProfile.createPublication.primaryButtonTitle}
+        title={t.editProfile.createPublication.title}
+      />
+    ),
+    [PostStatus.Cropping]: () => <div>Cropping</div>,
+    [PostStatus.Filter]: () => (
+      <FilterPhotoCard
+        items={[
+          {
+            filter: 'normal',
+            src: 'https://images.pexels.com/photos/17756265/pexels-photo-17756265.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+          },
+        ]}
+        onNextClick={() => {
+          setPostStatus(PostStatus.Publication);
+        }}
+        onPrevClick={() => {
+          setFiles([]);
+          setPostStatus(PostStatus.Init);
+        }}
+      />
+    ),
+    [PostStatus.Publication]: () => <div>Publication</div>,
+  };
+  /* eslint-enable perfectionist/sort-objects */
+
+  //<FilterPhotoCard items={[{ filter: 'valencia', src: '' }]} />
+
   return (
     <Modal.Root classes={{ content: s.modalContent }} onOpenChange={closeModalHandler} open={state}>
-      <ModalCard.Root className={s.cardRoot}>
-        <AddPhotoCard
-          error={null}
-          onClose={closeModalHandler}
-          primaryButtonTitle={t.editProfile.createPublication.primaryButtonTitle}
-          title={t.editProfile.createPublication.title}
-        />
-      </ModalCard.Root>
+      <ModalCard.Root className={s.cardRoot}>{steps[postStatus]()}</ModalCard.Root>
     </Modal.Root>
   );
 };
