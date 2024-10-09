4bdf5db7ec22423b59bc6a99ac4482d96a25f8ae
Stas-Lob4 <staslob4uck@gmail.com>
2024-10-09 11:28:50 +0200

    feat: create Sidebar - UI component

--- widgets/sidebar/sidebar.tsx
+++ widgets/sidebar/sidebar.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 
 import { ConfirmModal } from '@/features/confirm-modal';
 import { useLogoutMutation, useRefreshTokenQuery } from '@/shared/api/auth-api';
 import { useLazyGetMeQuery } from '@/shared/api/users-api';
 import { useTranslation } from '@/shared/hooks/useTranslation';
 import { Typography } from '@/shared/ui';
+import { UiSidebar } from '@/shared/ui/sidebar';
 import BookmarkOutline from 'assets/icons/bookmark-outline.svg';
 import HomeOutline from 'assets/icons/home-outline.svg';
 import LogOutOutline from 'assets/icons/log-out-outline.svg';
@@ -13,16 +14,15 @@ import PersonOutline from 'assets/icons/person-outline.svg';
 import PlusSquareOutline from 'assets/icons/plus-square-outline.svg';
 import SearchOutline from 'assets/icons/searchOutline.svg';
 import TrendingUpOutline from 'assets/icons/trending-up-outline.svg';
-import clsx from 'clsx';
 import Link from 'next/link';
-
-import s from './sidebar.module.scss';
+import { useRouter } from 'next/router';
 
 import { Trans } from '../Trans/Trans';
 
 export const Sidebar = () => {
   const t = useTranslation().t.common;
   const [open, setOpen] = useState(false);
+  const { pathname } = useRouter();
 
   const [logoutTrigger, { isLoading: isLogOutLoading }] = useLogoutMutation();
   const { isSuccess: isAuthSuccess } = useRefreshTokenQuery();
@@ -42,57 +42,56 @@ export const Sidebar = () => {
       console.error('Failed to delete avatar:', error);
     }
   };
+
   const closeModal = () => {
     if (!isLogOutLoading) {
       setOpen(false);
     }
   };
 
+  type SidebarSectionsData = {
+    [key: string]: {
+      disabled?: boolean;
+      href: string;
+      icon: ReactNode;
+      name: string;
+    }[];
+  };
+
+  const SECTIONS: SidebarSectionsData = {
+    section_01: [
+      { href: '/', icon: <HomeOutline />, name: 'Home' },
+      { disabled: true, href: '/create', icon: <PlusSquareOutline />, name: 'Create' },
+      { href: '/my-profile', icon: <PersonOutline />, name: 'My Profile' },
+      { disabled: true, href: '/messenger', icon: <MessageCircleOutline />, name: 'Messenger' },
+      { disabled: true, href: '/search', icon: <SearchOutline />, name: 'Search' },
+    ],
+    section_02: [
+      { disabled: true, href: '/statistics', icon: <TrendingUpOutline />, name: 'Statistics' },
+      { disabled: true, href: '/favorits', icon: <BookmarkOutline />, name: 'Favorites' },
+    ],
+  };
+
   return (
-    <div className={s.container}>
-      <div className={s.buttonsGroup}>
-        <Typography.Regular14 className={s.button} component={Link} href={'/'}>
-          <HomeOutline className={s.svg} />
-          Home
-        </Typography.Regular14>
-
-        <Typography.Regular14 className={s.button} tabIndex={1}>
-          <PlusSquareOutline className={s.svg} />
-          Create
-        </Typography.Regular14>
-        <Typography.Regular14 className={s.button} component={Link} href={'/my-profile'}>
-          <PersonOutline className={s.svg} />
-          My Profile
-        </Typography.Regular14>
-        <Typography.Regular14 className={s.button} tabIndex={1}>
-          <MessageCircleOutline className={s.svg} />
-          Messenger
-        </Typography.Regular14>
-        <Typography.Regular14 className={s.button} tabIndex={1}>
-          <SearchOutline className={s.svg} />
-          Search
-        </Typography.Regular14>
-      </div>
-
-      <div className={s.buttonsGroup}>
-        <Typography.Regular14 className={s.button} tabIndex={1}>
-          <TrendingUpOutline className={s.svg} />
-          Statistics
-        </Typography.Regular14>
-        <Typography.Regular14 className={s.button} tabIndex={1}>
-          <BookmarkOutline className={s.svg} />
-          Favorites
-        </Typography.Regular14>
-      </div>
-
-      <Typography.Regular14
-        className={clsx(s.button, s.buttonLogout)}
-        onClick={() => setOpen(true)}
-        tabIndex={1}
-      >
-        <LogOutOutline className={s.svg} />
-        Log Out
-      </Typography.Regular14>
+    <UiSidebar.Root>
+      {Object.entries(SECTIONS).map(([sectionKey, pages]) => (
+        <UiSidebar.Section key={sectionKey}>
+          {pages.map((page, index) => (
+            <UiSidebar.Item
+              component={Link}
+              disabled={page?.disabled}
+              href={page.href}
+              icon={page.icon}
+              isActive={pathname === page.href}
+              key={index}
+              title={page.name}
+            />
+          ))}
+        </UiSidebar.Section>
+      ))}
+      <UiSidebar.Footer>
+        <UiSidebar.Item icon={<LogOutOutline />} onClick={() => setOpen(true)} title={'Log out'} />
+      </UiSidebar.Footer>
 
       <ConfirmModal
         cancelButtonTitle={t.modal.buttonNames.cancel}
@@ -112,6 +111,6 @@ export const Sidebar = () => {
           />
         </Typography.Regular14>
       </ConfirmModal>
-    </div>
+    </UiSidebar.Root>
   );
 };
