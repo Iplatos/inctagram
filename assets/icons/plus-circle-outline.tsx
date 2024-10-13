bf7b8b15b963b22b5666b44f6de9201db748d35d
ArtyomSamsonchik <colpachek@gmail.com>
2024-10-13 19:42:11 +0300

    fix: `plus-circle-outline` icon appearance
    Now the icon inherits the color, as it should be.

--- assets/icons/plus-circle-outline.tsx
+++ assets/icons/plus-circle-outline.tsx
@@ -11,7 +11,7 @@ export const PlusCircleOutline = memo(
       xmlns={'http://www.w3.org/2000/svg'}
       {...props}
     >
-      <g clipPath={'url(#a)'} fill={'#000'}>
+      <g clipPath={'url(#a)'}>
         <path
           d={'M12 2a10 10 0 1 0 0 20 10 10 0 0 0 0-20Zm0 18a8 8 0 1 1 0-16.001A8 8 0 0 1 12 20Z'}
         />
