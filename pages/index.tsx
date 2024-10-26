2c99959a0fbcb7e8ef0ba4ef2d8ad1f9c564d54c
Marina <marina1497@list.ru>
2024-10-26 14:23:41 +0300

    fix: change dispatch to custom dispatch

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,6 @@ import {
   selectCreatePostModalOpen,
 } from '@/shared/api/modal-slice';
 import { useAppDispatch } from '@/shared/api/pretyped-redux-hooks';
-import { useAppSelector } from '@/shared/api/store';
 import { Button } from '@/shared/ui';
 import { HeadMeta } from '@/widgets/HeadMeta/HeadMeta';
 import { getLayout } from '@/widgets/Layout/Layout';
