96a396c8cd6fa1d28f7c2a57abd3298dc641486f
ArtyomSamsonchik <colpachek@gmail.com>
2024-10-29 08:33:22 +0300

    refactor: `EditPostModalCard`
    Component styles have been improved.
    Adjusted component API to make it fully controllable.
    Adjusted stories.
    Removed unnecessary use of `EditPostCard` from `ProfilePostCard`.

--- features/post/edit-post-modal-card/edit-post-modal-card.stories.tsx
+++ features/post/edit-post-modal-card/edit-post-modal-card.stories.tsx
@@ -1,20 +1,24 @@
-import { EditPostModalCard } from '@/features/post';
+import { EditPostModalCard, EditPostModalCardProps } from '@/features/post';
 import { action } from '@storybook/addon-actions';
 import { Meta, StoryObj } from '@storybook/react';
 
-const meta: Meta<typeof EditPostModalCard> = {
+/**
+`EditPostModalCard` is a component that displays a modal window for editing a post.
+The modal includes a form for post editing, a photo gallery (viewing only),
+and the user’s avatar.
+The user can submit changes to their post and close the modal
+either via the "Close" button or through confirmation in a secondary modal.
+
+### Key Features:
+- Post editing form.
+- Photo gallery for viewing images (no editing or selection functionality).
+- Submit button for saving changes and closing the modal.
+ */
+const meta = {
   argTypes: {
     avatar: {
       control: { type: 'text' },
       description: 'URL of the user’s avatar or a static image import.',
-    },
-    open: {
-      control: { type: 'boolean' },
-      description: 'Boolean flag to control the open/close state of the modal.',
-    },
-    setOpen: {
-      action: 'setOpen',
-      description: 'Function to handle the state of the modal (open/close).',
     },
     userName: {
       control: { type: 'text' },
@@ -22,51 +26,50 @@ const meta: Meta<typeof EditPostModalCard> = {
     },
   },
   component: EditPostModalCard,
-  parameters: {
-    docs: {
-      description: {
-        component: `
-\`EditPostModalCard\` is a component that displays a modal window for editing a post. The modal includes a form for post editing, a photo gallery (viewing only), and the user’s avatar. The user can submit changes to their post and close the modal either via the "Close" button or through confirmation in a secondary modal.
-
-### Key Features:
-- Post editing form.
-- Photo gallery for viewing images (no editing or selection functionality).
-- Submit button for saving changes and closing the modal.
-        `,
-      },
-    },
-  },
+  decorators: [
+    Story => (
+      <div style={{ height: 'calc(100vh - 2rem' }}>
+        <Story />
+      </div>
+    ),
+  ],
+  tags: ['autodocs'],
   title: 'features/post/EditPostModalCard',
-};
+} satisfies Meta<typeof EditPostModalCard>;
 
 export default meta;
-type Story = StoryObj<typeof EditPostModalCard>;
+type Story = StoryObj<typeof meta>;
+
+const editPostFormProps: EditPostModalCardProps['editPostFormProps'] = {
+  onSubmit: action('onSubmit'),
+  onSubmitError: action('onSubmitError'),
+  textLimit: 50,
+};
 
 export const Default: Story = {
   args: {
     avatar:
       'https://images.pexels.com/photos/27308308/pexels-photo-27308308.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    open: true,
-    setOpen: action('setOpen'),
+    editPostFormProps,
+    title: 'Edit Post',
     userName: 'John Doe',
   },
 };
 
 export const WithoutAvatar: Story = {
   args: {
+    ...Default.args,
     avatar: undefined,
-    open: true,
-    setOpen: action('setOpen'),
-    userName: 'Jane Doe',
   },
 };
 
-export const ClosedModal: Story = {
+export const WithError: Story = {
   args: {
-    avatar:
-      'https://images.pexels.com/photos/27308308/pexels-photo-27308308.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    open: false,
-    setOpen: action('setOpen'),
-    userName: 'John Doe',
+    ...Default.args,
+    editPostFormProps: {
+      ...editPostFormProps,
+      // cSpell: disable-next-line
+      description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt, ut cumque. Illo deserunt neque quo quasi, sapiente aut quibusdam eaque doloremque itaque facilis architecto nesciunt reprehenderit. Eos omnis mollitia earum recusandae nulla. Cupiditate nihil sequi corrupti iste accusantium illo consequuntur ab reprehenderit. Temporibus, quidem atque consectetur rerum voluptate ullam ad modi ut nobis voluptatum reiciendis necessitatibus omnis animi sequi eveniet quod ratione excepturi voluptates, rem maiores culpa iste, cum odio natus! Obcaecati quia magnam pariatur natus nemo porro illum, eum eius accusamus esse reiciendis accusantium sapiente, unde voluptate inventore deleniti dolorem quo voluptatum. Aut dignissimos maxime quod fuga! Assumenda, nisi!`,
+    },
   },
 };
--- features/post/edit-post-modal-card/edit-post-modal-card.tsx
+++ features/post/edit-post-modal-card/edit-post-modal-card.tsx
@@ -1,50 +1,36 @@
-import React, { useState } from 'react';
+import { FC, useId } from 'react';
 
 import { CloseIcon } from '@/assets/icons/close';
 import { PhotoGallery } from '@/features';
-import { ConfirmModal } from '@/features/confirm-modal';
-import { EditPostForm } from '@/features/post/edit-post-form/edit-post-form';
-import { useTranslation } from '@/shared/hooks/useTranslation';
-import { Button, Card, IconButton, Modal, Typography, UserBanner } from '@/shared/ui';
+import { EditPostForm, EditPostFormProps } from '@/features/post/edit-post-form/edit-post-form';
+import { Button, IconButton, ModalCard, Typography, UserBanner } from '@/shared/ui';
+import clsx from 'clsx';
 import { StaticImport } from 'next/dist/shared/lib/get-img-props';
 
 import s from './edit-post-modal-card.module.scss';
+import modalCardS from '@/shared/ui/modal-card/modal-card.module.scss';
 
-type Props = {
+export type EditPostModalCardProps = {
   avatar?: StaticImport | string;
-  open: boolean;
-  setOpen: (open: boolean) => void;
+  editPostFormProps: Pick<
+    EditPostFormProps,
+    'description' | 'onSubmit' | 'onSubmitError' | 'textFieldProps' | 'textLimit'
+  >;
+  onClose?: () => void;
+  submitButtonTitle?: string;
+  title: string;
   userName: string;
 };
 
-export const EditPostModalCard = (props: Props) => {
-  const { avatar, open, setOpen, userName } = props;
-  const { t } = useTranslation();
-  const [openConfirmModal, setOpenConfirmModal] = useState<boolean>(false);
-
-  const {
-    descriptionCloseModal,
-    labelCard,
-    postDescription: { titleBtnSubmit },
-  } = t.post.editPostModalCard;
-
-  const labelCloseModal = 'Close Post';
-  const formRef = React.useRef<HTMLFormElement>(null);
-
-  const onCloseEditPostModalCard = () => setOpen(false);
-  const onOpenConfirmModal = () => setOpenConfirmModal(true);
-  const onCloseConfirmModal = () => setOpenConfirmModal(false);
-
-  const handleSubmit = (data: { description: string }) => {
-    setOpen(false);
-    console.log(data);
-  };
-
-  const handleButtonClick = () => {
-    if (formRef.current) {
-      formRef.current.requestSubmit();
-    }
-  };
+export const EditPostModalCard: FC<EditPostModalCardProps> = ({
+  avatar,
+  editPostFormProps,
+  onClose,
+  submitButtonTitle = 'Save Changes',
+  title,
+  userName,
+}) => {
+  const formId = useId();
 
   const img = [
     'https://images.pexels.com/photos/27308308/pexels-photo-27308308.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
@@ -53,41 +39,26 @@ export const EditPostModalCard = (props: Props) => {
   ];
 
   return (
-    <Card.Root className={s.root}>
-      <Card.Header className={s.header}>
-        <Typography.H1 component={'h2'}>{labelCard}</Typography.H1>
-        <ConfirmModal
-          headerTitle={labelCloseModal}
-          onCancel={onCloseConfirmModal}
-          onConfirm={onCloseEditPostModalCard}
-          // open={openConfirmModal}
-          trigger={
-            <IconButton onClick={onOpenConfirmModal}>
-              <CloseIcon />
-            </IconButton>
-          }
-        >
-          <Typography.Regular16 className={s.confirmModal}>
-            {descriptionCloseModal}
-          </Typography.Regular16>
-        </ConfirmModal>
-      </Card.Header>
-      <Card.Content className={s.content} ignoreHeader>
+    <ModalCard.Root className={s.cardRoot}>
+      <ModalCard.Header className={s.header}>
+        <Typography.H1 className={modalCardS.headerTitle} component={'h2'}>
+          {title}
+        </Typography.H1>
+        <IconButton className={modalCardS.headerIconButtonLast} onClick={onClose}>
+          <CloseIcon />
+        </IconButton>
+      </ModalCard.Header>
+
+      <div className={s.contentWrapper}>
         <PhotoGallery additionalClass={s.gallery} items={img.map(i => ({ original: i }))} />
-        <div className={s.form}>
+        <ModalCard.Content className={clsx(s.content, modalCardS.contentScrollable)}>
           <UserBanner avatarProps={{ src: avatar }} userName={userName} />
-          <EditPostForm
-            actions={
-              <Button className={s.submitBtn} onClick={handleButtonClick}>
-                {titleBtnSubmit}
-              </Button>
-            }
-            classNameActions={s.actions}
-            onSubmit={handleSubmit}
-            ref={formRef}
-          />
-        </div>
-      </Card.Content>
-    </Card.Root>
+          <EditPostForm id={formId} {...editPostFormProps} />
+          <Button className={s.submitButton} form={formId}>
+            {submitButtonTitle}
+          </Button>
+        </ModalCard.Content>
+      </div>
+    </ModalCard.Root>
   );
 };
